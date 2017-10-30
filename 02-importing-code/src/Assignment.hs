61a44ea71e49cbf8e02cc30411ba934e1f63c3b0
Mike J Hewitt <mikeh@electroteach.com>
2017-10-30 20:16:01 +1100

    aeson sample

--- 02-importing-code/src/Assignment.hs
+++ 02-importing-code/src/Assignment.hs
@@ -1,10 +1,15 @@
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+{-
 module Assignment where
+-}
 
 import Data.Aeson (encode)
 
 list :: [Int]
-list = [1,2,3,4]
+list = [1, 2, 3, 4]
 
 main :: IO ()
 main = do
-    print (encode list)
+  print (encode list)
+  print "---"
+  print list
--- 02-importing-code/src/Main.hs
+++ 02-importing-code/src/Main.hs
@@ -1,25 +1,24 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
 module Main where
 
-import Data.Time (getCurrentTime, gregorianMonthLength, isLeapYear )
+import Data.Time (getCurrentTime, gregorianMonthLength, isLeapYear)
 import System.IO (readFile)
 
 main = do
-    putStrLn (greet "bobby")
-    putStrLn (greet "World")
-    printTime
-    printNumbers
-    printConfig
-    printConfig2
+  putStrLn (greet "bobby")
+  putStrLn (greet "World")
+  printTime
+  printNumbers
+  printConfig
+  printConfig2
  --   print isLeapYear 2017
-    
-    
 
 greet :: [Char] -> [Char]
 greet name = "Hello " ++ name ++ "!"
 
 printNumbers = do
-  putStrLn (show (3+4))
+  putStrLn (show (3 + 4))
 
 printConfig = do
   contents <- readFile "../stack.yaml"
@@ -32,14 +31,15 @@ printConfig2 = do
 printTime = do
   time <- getCurrentTime
   putStrLn (show time)
+
 {-
 https://hackage.haskell.org/package/time-1.8.0.3/docs/Data-Time-Clock.html
 getCurrentTime :: IO UTCTime
 
 Get the current UTCTime from the system clock.
 -}
-printTime2 = do
+printTime2
 --  time <- gregorianMonthLength (2017, 10)
 --  putStrLn (show time)
-    print 5
-    
+ = do
+  print 5
