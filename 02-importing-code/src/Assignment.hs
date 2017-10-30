6319af915a97e39aab7d1b6491930b83914d969d
Mike J Hewitt <mikeh@electroteach.com>
2017-10-30 20:40:38 +1100

    tests

--- 02-importing-code/src/Assignment.hs
+++ 02-importing-code/src/Assignment.hs
@@ -1,15 +1,34 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
+--try compilation w/othe following line =>failure
+{-# LANGUAGE DeriveGeneric #-}
 {-
 module Assignment where
 -}
 
+import Data.Aeson
+import Data.ByteString.Lazy.Char8
+import GHC.Generics
 import Data.Aeson (encode)
+
+data MyData = MyData { text :: String, number :: Int } deriving (Show, Generic)
+
+instance FromJSON MyData
+instance ToJSON MyData
+
+myData :: MyData
+myData = MyData "Hello" 123
 
 list :: [Int]
 list = [1, 2, 3, 4]
+
+
 
 main :: IO ()
 main = do
   print (encode list)
   print "---"
   print list
+  print myData
+  print $ unpack $ encode myData
+  print $ encode myData
